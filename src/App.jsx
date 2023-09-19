804687c4068195dadd91eeb8ff08fe3ced55e09d
merndevreza <merndevreza@gmail.com>
2023-09-19 11:58:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./assets/components/Hero/Hero";
+import Navbar from "./assets/components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
